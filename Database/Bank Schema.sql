57f80b738ff19b481d5b492a24cfe052406d817f
Suchitha Lochana <suchithald@gmail.com>
2018-11-27 21:43:45 +0530

    Current database full script
    Signed-off-by: Suchitha Lochana <suchithald@gmail.com>

--- Database/Bank Schema.sql
+++ Database/Bank Schema.sql
@@ -321,15 +321,13 @@ DELIMITER $$
 
 /*Procedure to do a standard withdrawal*/
 CREATE PROCEDURE standard_withdraw_transaction(
+  t_id VARCHAR(10),
   acc_no INT(10),
   withraw_amount DECIMAL(11,2),
   branch_ID VARCHAR(4) ) MODIFIES SQL DATA
   BEGIN
-    DECLARE t_id VARCHAR(10);
     DECLARE transaction_error BOOL DEFAULT 0;
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION SET transaction_error = 1;
-
-    SET t_id = '';
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET transaction_error = 1;
 
     SET AUTOCOMMIT = 0;
     START TRANSACTION;
@@ -353,21 +351,21 @@ CREATE PROCEDURE standard_withdraw_transaction(
       COMMIT ;
     END IF ;
 
+    SET AUTOCOMMIT = 1;
+
   END $$
 
 /*Procedure to do an ATM withdrawal*/
 CREATE PROCEDURE ATM_withdraw_transaction(
+  t_id VARCHAR(10),
   acc_no INT(10),
   withraw_amount DECIMAL(11,2),
   branch_ID VARCHAR(4) ) MODIFIES SQL DATA
   BEGIN
-    DECLARE t_id VARCHAR(10);
-    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
       ROLLBACK;
     END;
-
-    SET t_id = '';
 
     SET AUTOCOMMIT = 0;
     START TRANSACTION;
@@ -387,21 +385,21 @@ CREATE PROCEDURE ATM_withdraw_transaction(
 
     COMMIT;
 
+    SET AUTOCOMMIT = 1;
+
   END $$
 
 /*Procedure to do a deposit*/
 CREATE PROCEDURE deposit_transaction(
+  t_id VARCHAR(10),
   acc_no INT(10),
   withraw_amount DECIMAL(11,2),
   branch_ID VARCHAR(4) ) MODIFIES SQL DATA
   BEGIN
-    DECLARE t_id VARCHAR(10);
-    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
       ROLLBACK;
     END;
-
-    SET t_id = '';
 
     SET AUTOCOMMIT = 0;
     START TRANSACTION;
@@ -418,27 +416,27 @@ CREATE PROCEDURE deposit_transaction(
 
     COMMIT;
 
+    SET AUTOCOMMIT = 1;
+
   END $$
 
 /*Procedure to do a transfer*/
 CREATE PROCEDURE transfer_transaction(
+  trans_ID VARCHAR(10),
+  from_transaction VARCHAR(10),
+  to_transaction VARCHAR(10),
   from_acc INT(10),
   to_acc INT(10),
   transfer_amount DECIMAL(11,2),
   branch_ID VARCHAR(4) ) MODIFIES SQL DATA
   BEGIN
-    DECLARE from_transaction VARCHAR(10);
-    DECLARE to_transaction VARCHAR(10);
     DECLARE curr_time TIMESTAMP;
 
-    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
       ROLLBACK;
     END;
 
-
-    SET from_transaction = '';
-    SET to_transaction = '';
     SET curr_time = CURRENT_TIMESTAMP;
 
     SET AUTOCOMMIT = 0;
@@ -459,6 +457,9 @@ CREATE PROCEDURE transfer_transaction(
     INSERT INTO Deposit
     VALUES (to_transaction);
 
+    INSERT INTO Transfer
+    VALUES (trans_ID, from_transaction, to_transaction);
+
     UPDATE Account
     SET balance = balance - transfer_amount
     WHERE account_no = from_acc;
@@ -468,6 +469,8 @@ CREATE PROCEDURE transfer_transaction(
     WHERE account_no = to_acc;
 
     COMMIT;
+
+    SET AUTOCOMMIT = 1;
 
   END $$
 
@@ -485,7 +488,7 @@ CREATE PROCEDURE online_loan_transaction(
   install     DECIMAL(11, 2),
   FDID   INT(10)) MODIFIES SQL DATA
   BEGIN
-    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
     BEGIN
       ROLLBACK;
     END;
@@ -503,6 +506,8 @@ CREATE PROCEDURE online_loan_transaction(
     VALUES (l_ID, FDID);
 
     COMMIT;
+
+    SET AUTOCOMMIT = 1;
 
   END $$
 
@@ -657,10 +662,8 @@ GRANT 'standard_privileges' TO 'customer'@'localhost';
 
 GRANT INSERT ON ATM_Withdrawal TO 'customer'@'localhost';
 GRANT INSERT ON Loan TO 'customer'@'localhost';
-GRANT INSERT ON Offline_Loan TO 'customer'@'localhost';
+GRANT INSERT ON Online_loan TO 'customer'@'localhost';
 GRANT DELETE ON Loan_Request TO 'customer'@'localhost';
-GRANT UPDATE ON Individual TO 'customer'@'localhost';
-GRANT UPDATE ON Organization TO 'customer'@'localhost';
 
 
 #Non managerial employee account and privileges
